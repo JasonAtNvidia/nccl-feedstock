500364c46d2266eecd3a7bd6c7fbe0a77369daa8
Jason Tichy <jtichy@nvidia.com>
2020-05-26 18:16:56 -0400

    Updating to build for the Jetson platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -j${CPU_COUNT} CUDA_HOME="${CUDA_HOME}" CUDARTLIB="cudart"
+make -j${CPU_COUNT} NVCC_GENCODE="-gencode=arch=compute_72,code=sm_72 -gencode=arch=compute_62,code=sm_62 -gencode=arch=compute_53,code=sm_53"
 make install PREFIX="${PREFIX}"
